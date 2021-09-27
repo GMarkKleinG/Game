4a51551c7695c76e118ec29a7e639d71141e75c8
GMarkKleinG <90606220+GMarkKleinG@users.noreply.github.com>
2021-09-27 11:22:59 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Monster":return img`
+. . . . . . . . . . . . 3 3 3 3 
+. . . . . . . . 3 b b 3 1 1 1 1 
+. . . . . . . 3 b d d 1 1 1 1 1 
+. . . . . . . b d d d d 1 1 1 1 
+. . . . . . 3 d 1 1 1 d d 1 1 1 
+. . . . . . 3 d 1 1 1 1 1 d d 1 
+. . . . b b 3 d 1 1 1 1 1 1 d d 
+. . . b 3 d 3 d d 1 1 1 1 1 1 d 
+. . . 3 d d d 3 1 d 1 1 1 1 1 1 
+. . 3 1 1 1 1 1 d 1 d d d 1 1 1 
+. . 3 1 1 1 1 1 1 d 1 1 1 d d 1 
+. . b d d d 1 1 1 1 d d d 1 1 d 
+. . 3 1 1 d d d 1 1 1 d d d d 1 
+. . 3 1 1 1 1 d d 1 1 1 d d d d 
+. . . b d 1 1 1 1 d 1 1 1 3 d d 
+. . . 4 d d 1 1 1 1 d 1 1 1 3 d 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,56 @@
+namespace SpriteKind {
+    export const Monster = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, null, function (sprite, undefined) {
+	
+})
+let Student: Image = null
+tiles.setTilemap(tilemap`level1`)
+scene.setBackgroundColor(7)
+Student = Student.image
+animation.runImageAnimation(
+Student,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `],
+100,
+true
+)
+scene.cameraFollowSprite(Student)
+controller.moveSprite(Student, 100, 100)
+let Teacher = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+Teacher.setPosition(136, 106)
+Teacher.setVelocity(50, 50)
+Teacher.follow(Student, 70)
